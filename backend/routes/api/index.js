d35d029431680960282094494edb1c45f4667e5b
sneakblue <sneakblue@gmail.com>
2023-04-20 15:43:45 -0600

    all auth routes working, removed test code

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,17 +1,5 @@
 const router = require('express').Router();
 
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth');
-const { User } = require('../../db/models');
 
-router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-    const user = await User.findOne({
-        where: {
-            username: 'Demo-lition'
-        },
-    })
-    setTokenCookie(res, user);
-    return res.json({ user });
-}));
 
 module.exports = router;
