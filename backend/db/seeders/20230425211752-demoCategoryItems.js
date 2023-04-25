143c1e4d0c5b2a595ceb3e4afe786748b885c963
sneakblue <sneakblue@gmail.com>
2023-04-25 15:27:53 -0600

    added first entry to catgeoryItems joins table seeder

--- backend/db/seeders/20230425211752-demoCategoryItems.js
+++ backend/db/seeders/20230425211752-demoCategoryItems.js
@@ -3,29 +3,15 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up (queryInterface, Sequelize) {
-        /**
-         * Add seed commands here.
-         *
-         * Example:
-         * await queryInterface.bulkInsert('People', [{
-         *   name: 'John Doe',
-         *   isBetaMember: false
-         * }], {});
-        */
         await queryInterface.bulkInsert('CategoryItems', [
             {
-
+                sub_category_id: 2,
+                store_item_id: 1,
             }
         ], {});
     },
 
     async down (queryInterface, Sequelize) {
-        /**
-         * Add commands to revert seed here.
-         *
-         * Example:
-         * await queryInterface.bulkDelete('People', null, {});
-         */
         await queryInterface.bulkDelete('CategoryItems', null, {});
     }
 };
