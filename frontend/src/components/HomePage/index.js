c3f608c4664c1214b75016c79e6883e9df205f95
sneakblue <sneakblue@gmail.com>
2023-04-26 22:51:23 -0600

    adding categories slice of state

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCategories } from '../../store/categories';
 import CategoryCard from './CategoryCard';
 import './HomePage.css';
 
@@ -20,6 +23,9 @@ const categories = [
 
 
 export default function HomePage() {
+    
+
+
     return (
         <div className="homePage_main_div">
             <h1>HomePage</h1>
---
+++ frontend/src/store/categories.js
@@ -0,0 +1,35 @@
+import { csrfFetch } from "./csrf";
+
+const GET_CATEGORIES = 'categories/get_categories'
+
+const setCategories = (categories) => {
+    return {
+        type: GET_CATEGORIES,
+        payload: categories
+    }
+}
+
+export const getCategories = () => async (dispatch) => {
+    const res = await csrfFetch('/api/categories/')
+
+    if (res.ok) {
+        const categories = await res.json();
+        dispatch(setCategories(categories));
+        return categories;
+    } else {
+        console.log('Oops! No categories!');
+    }
+}
+
+export default function categoriesReducer(state = {}, action) {
+    let newState;
+    switch(action.type) {
+        case GET_CATEGORIES: {
+            newState = {...state};
+            action.payload.forEach(category => newState[category.id] = category);
+            return newState;
+        }
+        default:
+            return state;
+    }
+}
