9c3bbe91bcd32d84a0e8d37ba013e1dfe2f39bf3
sneakblue <sneakblue@gmail.com>
2023-06-22 16:34:32 -0500

    item list functional and styled

--- frontend/src/components/SubCategoryPage/ItemCard.js
+++ frontend/src/components/SubCategoryPage/ItemCard.js
@@ -5,13 +5,19 @@ export default function ItemCard({item}) {
 
     return (
         <div className="subCategory-item-card--div" key={item.id}>
-            <img src={item.imageUrl} className="subCategory-item-card--img"/>
+            <div className='subCategory-item-card-img--div'>
+                <Link to={`/item/${item.id}`}>
+                    <img src={item.imageUrl} className="subCategory-item-card--img"/>
+                </Link>
+            </div>
             <div className="subCategory-item-card-details--div">
                 <div className="subCategory-item-card-name--div">
-                    <p>{item.name}</p>
+                    <Link to={`/item/${item.id}`}>
+                        <p className='subCategory-item-card-name--p'>{item.name}</p>
+                    </Link>
                 </div>
                 <div>
-                    <p>{`$${item.price}`}</p>
+                    <p className='subCategory-item-card-price--p'>{`$${item.price}`}</p>
                 </div>
             </div>
         </div>
