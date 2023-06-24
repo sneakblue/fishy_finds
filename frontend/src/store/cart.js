15e6e2f570610133430f3d5aba0d6cc4124e60d8
sneakblue <sneakblue@gmail.com>
2023-06-24 18:00:11 -0500

    working initial cart load/check

--- frontend/src/store/cart.js
+++ frontend/src/store/cart.js
@@ -1,5 +1,28 @@
+
+const GET_CART_ITEMS = 'cart/load_items';
+
+const setCartItems = (cartItems) => {
+    return {
+        type: GET_CART_ITEMS,
+        payload: cartItems
+    }
+}
+
+export const getCartItems = (userId) => async (dispatch) => {
+    if (userId) {
+
+    } else {
+        
+    }
+}
+
 export default function cartReducer(state = {}, action) {
     switch(action.type) {
+        case GET_CART_ITEMS: {
+            let newState = {};
+            action.payload.forEach(item => newState[item.id] = item);
+            return newState;
+        }
         default:
             return state;
     }
