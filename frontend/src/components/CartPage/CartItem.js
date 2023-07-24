35d5fc403ee22660c2387c7179f6e1c60f9938f3
sneakblue <sneakblue@gmail.com>
2023-07-24 17:10:10 -0600

    minor styling on cart page

--- frontend/src/components/CartPage/CartItem.js
+++ frontend/src/components/CartPage/CartItem.js
@@ -1,33 +1,54 @@
 import { useDispatch, useSelector } from "react-redux";
 import { updateCartItem } from "../../store/cart";
+import { useState } from "react";
 
 export default function CartItem ({item}) {
     const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user);
+    const [ quantity, setQuantity ] = useState(item.quantity);
 
-    const removeItemHandler = (id) => {
-        console.log(id)
-        dispatch(updateCartItem(sessionUser, id, 0));
+    const removeItemHandler = () => {
+        // console.log(id)
+        dispatch(updateCartItem(sessionUser, item.id, 0));
+    }
+
+    const updateQuantityHandler = () => {
+        dispatch(updateCartItem(sessionUser, item.id, quantity));
     }
 
     return (
         <div className="cartList-item-container--div">
-            <div className="carList-item-img-title--div">
-                <img
-                    className='cartPage-item--img'
-                    src={item?.imageUrl}
-                    alt='product'
-                />
-                <div>
-                    <h5>{item.name}</h5>
-                    <button onClick={() => removeItemHandler(item.id)}>Remove</button>
-                </div>
-                <div>
-                    <h4>Quantity: {item.quantity}</h4>
-                    <input
-
+            <div className="cartList-item-img-title--div">
+                <div className='cartList-item-img-container--div'>
+                    <img
+                        className='cartPage-item--img'
+                        src={item?.imageUrl}
+                        alt='product'
                     />
                 </div>
+                <div className='cartList-item-title--div'>
+                    <h5 className='cartList-item-name--header'>{item.name}</h5>
+                    <button
+                        className="cartList-item-remove--buton"
+                        onClick={() => removeItemHandler()}
+                    >Remove</button>
+                </div>
+                <div className='cartList-item-quantity--div'>
+                    <h5>Quantity: </h5>
+                    <input
+                        type='number'
+                        className="cartList-item-quantity--input"
+                        value={quantity}
+                        onChange={(e) => setQuantity(e.target.value)}
+                        onBlur={updateQuantityHandler}
+                    />
+                </div>
+            </div>
+            <div className="cartList-price--div">
+                <h4>Price: ${item.price}</h4>
+            </div>
+            <div className="cartList-subTotal--div">
+                <h4>SubTotal: ${(item.price * quantity).toFixed(2)}</h4>
             </div>
         </div>
     )
--- frontend/src/components/CartPage/CartList.js
+++ frontend/src/components/CartPage/CartList.js
@@ -17,7 +17,7 @@ export default function CartList({cartItemDetails}) {
 
     return (
         <div className="cartList-main-container--div">
-            {cartItemDetails.map((item) => <CartItem item={item} />)}
+            {cartItemDetails.map((item) => <CartItem item={item} key={item.id} />)}
         </div>
     )
 }
