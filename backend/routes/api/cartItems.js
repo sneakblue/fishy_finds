1bc75da51b8a3739920138ecfddff29230151075
sneakblue <sneakblue@gmail.com>
2023-07-24 22:19:01 -0600

    built simple routes for adding, fetching items

--- backend/routes/api/cartItems.js
+++ backend/routes/api/cartItems.js
@@ -1,9 +1,45 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-const { CartItem } = require('../../db/models');
+const { CartItem, StoreItem } = require('../../db/models');
 
 const router = express.Router();
 
+router.post('/addItem', asyncHandler( async (req, res) => {
+    const { user_id, item_id, quantity } = req.body;
+    const updateItem = await CartItem.findOne({
+        where: {
+            user_id,
+            item_id
+        }
+    });
+    if (updateItem) {
+        updateItem.update({
+            quantity
+        });
 
+    } else {
+        await CartItem.create({
+            user_id,
+            item_id,
+            quantity
+        });
+    }
+    const userCartItem = await StoreItem.findByPk(item_id);
+    userCartItem.quantity = quantity;
+    return res.json(userCartItem);
+
+}))
+
+// Get all cart items for a single User
+router.get('/:userId', asyncHandler( async (req, res) => {
+    const { userId } = req.params;
+    const userItems = await CartItem.findAll({
+        where: {
+            user_id: userId
+        },
+        include: StoreItem
+    });
+
+}));
 
 module.exports = router;
