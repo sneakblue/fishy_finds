bda611569b06d0804bf1051e53875f62d53ac04c
sneakblue <sneakblue@gmail.com>
2023-08-08 23:10:58 -0600

    added fetch of category state to category page

--- frontend/src/components/CategoryPage/index.js
+++ frontend/src/components/CategoryPage/index.js
@@ -1,10 +1,21 @@
-// import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getCategories } from '../../store/categories';
+import './CategoryPage.css';
 
 export default function CategoryPage() {
-    // const { id } = useParams();
+    const dispatch = useDispatch();
+    const { id } = useParams();
+    const category = useSelector(state => state.categories[id]);
+    console.log(category);
+    useEffect(() => {
+        dispatch(getCategories());
+    }, [])
     return (
-        <>
-            <h2>Category Page</h2>
-        </>
+        <div className='categoryPage-main-container--div'>
+            <div className='navbar-spacer'/>
+            <h2>{category?.title}</h2>
+        </div>
     )
 }
