529f855291e00feba231109305b33bf442c00e00
sneakblue <sneakblue@gmail.com>
2023-08-08 22:36:03 -0600

    working category navbar with minimum styling

--- frontend/src/components/Navigation/CategoryNavModal.js
+++ frontend/src/components/Navigation/CategoryNavModal.js
@@ -1,8 +1,13 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from 'react-router-dom';
 import { CategoryModal } from "../../context/Modal";
+import './Navigation.css';
 
 export default function CatgoryNavModal({}) {
     const [ showModal, setShowModal ] = useState(false);
+    const categories = useSelector(state => Object.values(state.categories));
+    console.log(categories)
 
     return (
         <>
@@ -11,7 +16,19 @@ export default function CatgoryNavModal({}) {
             </div>
             {showModal && (
                 <CategoryModal onClose={(() => setShowModal(false))}>
-                    
+                    <div className="category-nav-modal--div">
+                        {categories.map(category => {
+                            return (
+                                <Link
+                                    to={`/category/${category.id}`}
+                                    className='category-nav--link'
+                                    onClick={() => setShowModal(false)}
+                                >
+                                    {category.title}
+                                </Link>
+                            )
+                        })}
+                    </div>
                 </CategoryModal>
             )}
         </>
